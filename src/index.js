5fabcb272e4cbb9badf1d7de03e043693ef704d0
Nilianys Bareno <nilianysbareno@gmail.com>
2020-05-28 00:00:04 -0500

    organización de los archivos e inicialización de sockets

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+
+
+//star files
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//start server 
+server.listen(8080, () => {
+    console.log('server on port 8080');
+});
