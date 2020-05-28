245d5e4a65e9d3d53462c28504c9ba86aeb8c929
Nilianys Bareno <nilianysbareno@gmail.com>
2020-05-28 18:24:53 -0500

    Actualización sockets del server para mostrar mensaje a todos los usuarios

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,20 @@
-io();
+$(function (){
+    
+    const socket = io();
+
+    //obtaining DOM elements from the interfece
+    const $messageFrom = $('#message-form');
+    const $messageBox = $('#message');
+    const $chat = $('#chat');
+
+    //events
+    $messageFrom.submit( e => {
+        e.preventDefault ();
+        socket.emit('send message', $messageBox.val());
+        $messageBox.val('');
+    });
+
+    socket.on('new message', function (data){
+        $chat.append(data + '<br/>'); 
+    });
+})
--- src/sockets.js
+++ src/sockets.js
@@ -2,6 +2,10 @@ module.exports = function (io){
 
     io.on('connection', socket => {
         console.log('new user connected');
+
+        socket.on('send message', function(data){
+            io.sockets.emit('new message', data);
+        });
     });
     
 }
